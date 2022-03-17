a3fdf1988e5d8e20515fc571c8b48bb111ea5582
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2022-03-16 20:42:51 -0400

    set up some get routes so the nav bar works is functional now

--- mongoose-express-liquid-boilerplate-main/controllers/meme.js
+++ mongoose-express-liquid-boilerplate-main/controllers/meme.js
@@ -3,6 +3,7 @@ const express = require('express')
 //const Example = require('../models/example')
 const FavMeme = require('../models/favMeme')
 const fetch = require('node-fetch')
+const { model } = require('../models/connection')
 
 
 // Create router
@@ -21,37 +22,104 @@ router.use((req, res, next) => {
 		res.redirect('/auth/login')
 	}
 })
+/////////////////////////////
+//Routes
+/////////////////////////////
 
-		////////////
-		router.get('/search', (req, res) => {
-            const { username, userId, loggedIn } = req.session
-               //res.send('hi')
-                   res.render('memes/search', {loggedIn, username, userId })
+//////////////
+//index that shows all memes
+//////////////
+router.get('/', (req,res) => {
+    //find all liked memes
+    FavMeme.find({})    
+        .then((memes) => {
+            const username = req.session.username
+            const loggedIn = req.session.loggedIn
+            res.render('memes/index', {memes, username, loggedIn})
+            //res.send('hi')
+        })
+        .catch((error) => {
+            console.log(error)
+            res.json({error})
+        })
+})
+//////////////
+//index that shows only user's memes
+//////////////
+router.get('/mine', (req,res) => {
+    FavMeme.find({username:req.session.username})
+        .then((memes) => {
+            const username = req.session.username
+            const loggedIn = req.session.loggedIn
+            res.render('memes/index', {memes, username, loggedIn})
+            //res.send('hi')
+        })
+        .catch((error) => {
+            console.log(error)
+            res.json({error})
+        })
+})
+//////////////
+//post for liking a meme that gets stored under username in /memes/mine
+//////////////
+router.post('/', (req,res) => {
+    req.body.username = req.session.username
+	FavMeme.create(req.body)
+		.then((meme) => {
+			console.log('this was returned from hitting like', meme)
+			res.redirect('/memes')
+		})
+		.catch((err) => {
+			console.log(err)
+			res.json({ err })
+		})
+})
+//////////////
+//
+//////////////
+
+//////////////
+//
+//////////////
+
+
+router.get('/search', (req, res) => {
+    console.log('this is my get route: memes/search ')
+    const { username, userId, loggedIn } = req.session
+       //res.send('hi')
+    res.render('memes/search', {loggedIn, username, userId })
            
-       })
+})
        
-       router.post('/search', (req, res) => {
-           const page = req.body.page
-           console.log('this is the page ', page)
-           res.redirect(`/memes/${page}`)  
-           
+router.post('/search', (req, res) => {
+    console.log('this is my post: /memes/${page}')
+    const page = req.body.page
+    console.log('this is the page ', page)
+    res.redirect(`/memes/search/${page}`)  
+
+ })
+
+ ////////////////
+ //show route - get route that takes us to specific meme page 
+ ///////////////
+router.get('/search/:memePage', (req, res) => {
+    console.log('this is my get: memes/searchShow')
+    const { username, userId, loggedIn } = req.session
+    const page = parseInt(req.params.memePage)
+    const requestURL = `http://alpha-meme-maker.herokuapp.com/${page}`
+     fetch(requestURL)
+        .then(responseData => {
+            return responseData.json()
         })
-        router.get('/:memePage', (req, res) => {
-            const { username, userId, loggedIn } = req.session
-            const page = parseInt(req.params.memePage)
-            const requestURL = `http://alpha-meme-maker.herokuapp.com/${page}`
-             fetch(requestURL)
-                .then(responseData => {
-                    return responseData.json()
-                })
-                .then(jsonMemeData => {
-                    console.log(jsonMemeData)
-                    res.render('memes/searchShow', {memeData: jsonMemeData, loggedIn, username, userId} )
-                })
-                .catch(error => {
-                    res.send(error)
-                })
+        .then(jsonMemeData => {
+            console.log(jsonMemeData)
+            res.render('memes/searchShow', {memeData: jsonMemeData, loggedIn, username, userId} )
         })
-        /////////////
-        // Export the Router
+        .catch(error => {
+            res.send(error)
+        })
+})
+/////////////
+// Export the Router
+////////////
 module.exports = router
